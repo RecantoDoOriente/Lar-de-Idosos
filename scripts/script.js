8763720e42393da2e9e1c3a4a032df42defc388b
RecantoDoOriente <102874108+RecantoDoOriente@users.noreply.github.com>
2022-04-10 18:56:42 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -5,13 +5,18 @@ function aparecerSumir(local, materia = '') {
 
 const btnmenu = document.querySelector('div.menu')
 const menuInt = document.querySelector('div.menuInt')
+const links = document.querySelector('div.links')
 
 btnmenu.addEventListener('click', () => {
     menuInt.classList.toggle('active')
+    links.classList.toggle('active')
+
 })
 
 const aNav = document.querySelectorAll('nav.small a')
 
 aNav.addEventListener('click', () => {
     menuInt.classList.remove('active')
+    links.classList.toggle('active')
+
 })
